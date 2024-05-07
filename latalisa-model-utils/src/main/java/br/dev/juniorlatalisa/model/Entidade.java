067b323cb3cb7bcb1a0ac19ebe4aaf82b7048756
juniorlatalisa <paraojuniorler@gmail.com>
2024-05-06 23:09:53 -0300

    Ajustes

--- latalisa-model-utils/src/main/java/br/dev/juniorlatalisa/model/Entidade.java
+++ latalisa-model-utils/src/main/java/br/dev/juniorlatalisa/model/Entidade.java
@@ -5,5 +5,8 @@ import java.io.Serializable;
 public interface Entidade extends Serializable {
 
 	String CURRENCY_COLUMN_DEFINITION = "NUMERIC(18,2)";
+	
+	String SELECT_FROM = "select e from ";
+	String SELECT_WHERE = " e where ";
 
 }
